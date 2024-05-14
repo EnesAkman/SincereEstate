17fc68528e239cc0a2f50f343f3b8646afe619e6
Enes Işık <30756694+isikenes@users.noreply.github.com>
2024-05-14 11:34:35 +0300

    validation

--- src/main/java/com/EEY/SincereEstate/entity/User.java
+++ src/main/java/com/EEY/SincereEstate/entity/User.java
@@ -2,6 +2,8 @@ package com.EEY.SincereEstate.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.context.annotation.Lazy;
 
@@ -17,18 +19,28 @@ public class User {
     @Column(name = "user_id")
     private int id;
 
+    @NotNull(message = "First name is required!")
+    @Size(min = 1, message ="First name is required")
     @Column(name = "first_name")
     private String firstName;
 
+    @NotNull(message = "Last name is required!")
+    @Size(min = 1, message ="Last name is required")
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
+    @NotNull(message = "Email is required!")
+    @Size(min = 1, message ="Email is required")
+    @Column(name = "email", unique = true)
     private String email;
 
+    @NotNull(message = "Password is required!")
+    @Size(min = 1, message ="Password is required")
     @Column(name = "password")
     private String password;
 
+    @NotNull(message = "Phone is required!")
+    @Size(min = 1, message ="Phone is required")
     @Column(name = "phone")
     private String phone;
 
